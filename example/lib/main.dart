1c1eb4d820a9adfe78e05b413d65fd869ce2aa5e
iosemagno <iose.magno@gmail.com>
2023-02-23 14:17:54 +0100

    Update main

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,7 +32,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late final CachedVideoPlayerController controller =
-      CachedVideoPlayerController.network(
+  CachedVideoPlayerController.network(
     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
     formatHint: VideoFormat.other,
     videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
@@ -74,24 +56,71 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+
+      body: Column(
+        children: [
+          const SizedBox(height: 8),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "Player with cache enabled.",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child: CachedVideoPlayer(controller)),
+          TextButton(
+            child: Text("Start pre cache"),
+            onPressed: () {
+              //video gets downloaded for 300 ms before playback.
+              //controller.preCache(300);
+              //precaching duration can also be hardcoded
+              //controller.preCache();
+            },
+          ),
+          TextButton(
+            //video playback shouldn't start automatically
+            child: Text("Play video"),
+            onPressed: () {
+              controller.play();
+            },
+          ),
+          TextButton(
+            child: Text("Pause video"),
+            onPressed: () {
+              controller.pause();
+            },
+          ),
+          TextButton(
+            child: Text("Stop video"),
+            onPressed: () {
+              //pause and move to first frame
+              //controller.stop();
+            },
+          ),
+          TextButton(
+            //clear url resource from DataSource
+            child: Text("Clear cache"),
+            onPressed: () {
+            },
+          ),
+        ],
+      ),
+
+      /*
       body: Center(
           child: controller.value.isInitialized
               ? AspectRatio(
                   aspectRatio: controller.value.aspectRatio,
                   child: CachedVideoPlayer(controller))
               : const CircularProgressIndicator()), // This trailing comma makes auto-formatting nicer for build methods.
+   */
+
     );
   }
 
